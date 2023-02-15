0e45fd0e9acf2228fd69a852e3fe76f505b031eb
Daniel Beneš <78357621+Danny-Benny@users.noreply.github.com>
2023-02-16 00:08:07 +0100

    [task] some formating

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,27 @@ import * as React from "react";
 import { Box, Container, Grid } from "@mui/material";
 import TrickCard from "@/components/card";
 
+const trickCards = [
+  {
+    id: "Ux1gvUdFzgw",
+    title: "Ollie",
+    description: "Prvni trik co se naucime",
+    trick_img_alt: "ollie",
+  },
+  {
+    id: "IK55jb8ebtw",
+    title: "Pop shove it",
+    description: "Druhy trik co se naucime",
+    trick_img_alt: "popshoveit",
+  },
+  {
+    id: "Yv_NXSEx_YY",
+    title: "Kickflip",
+    description: "Treti trik co se naucime",
+    trick_img_alt: "kickflip",
+  },
+];
+
 const Home: React.FC = () => {
   return (
     <>
@@ -13,54 +34,16 @@ const Home: React.FC = () => {
             columnSpacing={{ xs: 1, sm: 2, md: 3 }}
             sx={{ paddingTop: "10px" }}
           >
-            <Grid item xs={3}>
-              <TrickCard
-                id="Ux1gvUdFzgw"
-                title="Ollie"
-                description="Prvni trik co se naucime"
-                trick_img_alt="ollie"
-              />
-            </Grid>
-            <Grid item xs={3}>
-              <TrickCard
-                id="IK55jb8ebtw"
-                title="Pop shove it"
-                description="Druhy trik co se naucime"
-                trick_img_alt="popshoveit"
-              />
-            </Grid>
-            <Grid item xs={3}>
-              <TrickCard
-                id="Yv_NXSEx_YY"
-                title="Kickflip"
-                description="Treti trik co se naucime"
-                trick_img_alt="kickflip"
-              />
-            </Grid>
-            <Grid item xs={3}>
-              <TrickCard
-                id="dQw4w9WgXcQ"
-                title="title"
-                description="description"
-                trick_img_alt="trick_img_alt"
-              />
-            </Grid>
-            <Grid item xs={3}>
-              <TrickCard
-                id="dQw4w9WgXcQ"
-                title="title"
-                description="description"
-                trick_img_alt="trick_img_alt"
-              />
-            </Grid>
-            <Grid item xs={3}>
-              <TrickCard
-                id="dQw4w9WgXcQ"
-                title="title"
-                description="description"
-                trick_img_alt="trick_img_alt"
-              />
-            </Grid>
+            {trickCards.map((trickCard, index) => (
+              <Grid item xs={3} key={index}>
+                <TrickCard
+                  id={trickCard.id}
+                  title={trickCard.title}
+                  description={trickCard.description}
+                  trick_img_alt={trickCard.trick_img_alt}
+                />
+              </Grid>
+            ))}
           </Grid>
         </Box>
       </Container>
